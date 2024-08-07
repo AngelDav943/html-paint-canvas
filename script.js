d5edfdbeb430fbea410c6fafd82886eb888589cf
Angel <angel92.addcs@gmail.com>
2024-08-07 15:13:24 -0500

    uhh made it better?

--- script.js
+++ script.js
@@ -6,46 +6,82 @@ const eraserInput = document.getElementById("eraser")
 const colorInput = document.getElementById("fillColor")
 
 let clicking = false;
-let lastPos = null
 
-function clearCanvas() {
-    context.clearRect(0,0, canvas.width, canvas.height)
-}
-
-function startClicking(event) {
-    clicking = true;
-}
-
-canvas.addEventListener("touchstart", startClicking);
-canvas.addEventListener("mousedown", startClicking);
+let lastPos = null;
+let lastTouches = {};
 
 function endClicking() {
     lastPos = null
     clicking = false;
 }
 
-canvas.addEventListener("touchcancel", endClicking);
-canvas.addEventListener("touchend", endClicking);
-canvas.addEventListener("mouseout", endClicking);
-canvas.addEventListener("mouseup", endClicking);
+function convertTouchToMouse(TouchEvent, returnFunction) {
+    if (TouchEvent == undefined) return
+    if (TouchEvent.touches == undefined || returnFunction == undefined) return
 
-function painting(event) {
+    for (const touch of TouchEvent.touches) {
+        const offset = {
+            offsetX: touch.clientX - canvas.offsetLeft,
+            offsetY: touch.clientY - canvas.offsetTop
+        }
+
+        const x = (offset.offsetX / canvas.offsetWidth) * canvas.width;
+        const y = (offset.offsetY / canvas.offsetHeight) * canvas.height;
+
+        lastTouches[event.identifier] = { y: y, x: x }
+
+        returnFunction({
+            ...touch,
+            identifier: touch.identifier,
+            force: touch.force,
+            ...offset
+        }, true)
+    }
+}
+
+function clearCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height)
+}
+
+function painting(event, isTouch) {
     if (clicking == false) return;
+
+    let identifier = -1
+    if (event.identifier != undefined) {
+        isTouch = true;
+        identifier = event.identifier
+    }
+    
+    let force = 1
+    if (event.force != undefined) force = event.force
+    
     const x = (event.offsetX / canvas.offsetWidth) * canvas.width;
     const y = (event.offsetY / canvas.offsetHeight) * canvas.height;
-    console.log("xy", x, y)
-
-    const radius = radiusInput.value
+    
+    const radius = radiusInput.value * force
     const erasing = eraserInput.checked
-
-    context.fillStyle = colorInput.value
-
-    // console.log(event.offsetX, event.offsetY);
-
+    
+    context.fillStyle = colorInput.value;
+    
+    function setLastPosition() {
+        if (isTouch == true) {
+            lastTouches[identifier] = { y: y, x: x }
+        } else {
+            lastPos = { y: y, x: x }
+        }
+    }
+    
+    let currentLast = undefined
+    if (isTouch == true) {
+        currentLast = lastTouches[identifier]
+    } else {
+        currentLast = lastPos
+    }
+    
     if (erasing) {
         context.clearRect(
             x - (radius), y - (radius),
-            radius*4, radius*4
+            radius * 4, radius * 4
         )
         return
     }
@@ -56,36 +92,57 @@ function painting(event) {
         context.fill();
     }
 
-    if (lastPos == null) {
+    if (currentLast == null) {
         drawPoint(x, y)
-
-        lastPos = { y: y, x: x }
+        setLastPosition()
         return
     }
+    
+    const diffX = x - currentLast.x
+    const diffY = y - currentLast.y
 
-    // lerp idk
-    const diffX = x - lastPos.x
-    const diffY = y - lastPos.y
-
-    const distance = Math.sqrt( diffX*diffX + diffY*diffY )
-    // console.log("distance", event.offsetX - lastPos.offsetX, event.offsetY - lastPos.offsetY)
-    // console.log(distance)
+    //const distance = Math.sqrt(diffX * diffX + diffY * diffY)
     for (let t = 0; t < 100; t++) {
         const time = t / 100
-        // console.log("t",time)
+        
         drawPoint(
-            (1 - time) * lastPos.x + (time * x),
-            (1 - time) * lastPos.y + (time * y)
+            (1 - time) * currentLast.x + (time * x),
+            (1 - time) * currentLast.y + (time * y)
         )
     }
 
-    lastPos = { y: y, x: x }
+    setLastPosition()
 }
 
-canvas.addEventListener("touchmove", (event) => {
-    console.log(event)
-    for (const touch of event.touches) {
-        console.log("touch", touch)
+function startClicking(event) {
+    clicking = true;
+    painting(event)
+}
+
+canvas.addEventListener("touchstart", (event) => convertTouchToMouse(event, startClicking));
+canvas.addEventListener("mousedown", startClicking);
+
+function endTouch(event) {
+    for (const touchIdentifier of Object.keys(lastTouches)) {
+        let exists = false
+        for (const touch of event.touches) {
+            if (touch.identifier == touchIdentifier) exists = true
+        }
+
+        if (exists == false) {
+            delete lastTouches[touchIdentifier]
+        }
     }
-});
+
+    if (event.touches.length < 1) {
+        endClicking();
+    }
+}
+canvas.addEventListener("touchend", endTouch );
+canvas.addEventListener("touchcancel", endTouch );
+
+canvas.addEventListener("mouseout", endClicking);
+canvas.addEventListener("mouseup", endClicking);
+
+canvas.addEventListener("touchmove", (event) => convertTouchToMouse(event, painting));
 canvas.addEventListener("mousemove", painting);
